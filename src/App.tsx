5761f88da4112e440e428f2a7147f17377469b20
radoslawgrochowski <rg@fard.pl>
2019-02-18 19:06:32 +0100

    adding idea's files to .gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-/*
-* asd asdasda Commit
-* */
 class App extends Component {
     render() {
         return (
