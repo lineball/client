9b80db492daf860864c45ef9a09102d74729f7c3
radoslawgrochowski <rg@fard.pl>
2019-02-19 18:38:09 +0100

    Updates

---
+++ precache-manifest.36c03423411d36f9b9b205713eb640c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8419729592cb480f537d",
+    "url": "/client/static/js/runtime~main.84197295.js"
+  },
+  {
+    "revision": "b21339018ea8457bc2e3",
+    "url": "/client/static/js/main.b2133901.chunk.js"
+  },
+  {
+    "revision": "54094b48b410ee26b80e",
+    "url": "/client/static/js/2.54094b48.chunk.js"
+  },
+  {
+    "revision": "b21339018ea8457bc2e3",
+    "url": "/client/static/css/main.8b0db3b9.chunk.css"
+  },
+  {
+    "revision": "07ddc3633d4e119932b4483c9b4a80d3",
+    "url": "/client/index.html"
+  }
+];
