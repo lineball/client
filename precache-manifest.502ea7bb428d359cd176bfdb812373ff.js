372c8b479002f12d4f72fcad058aaba3fdce9a4c
circleci <lineball@circleci.com>
2019-03-03 14:59:29 +0000

    [skip ci] Deploy with CircleCI

---
+++ precache-manifest.502ea7bb428d359cd176bfdb812373ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d24991506825ca650d88",
+    "url": "/client/static/css/main.a0056c84.chunk.css"
+  },
+  {
+    "revision": "d24991506825ca650d88",
+    "url": "/client/static/js/main.d2499150.chunk.js"
+  },
+  {
+    "revision": "8419729592cb480f537d",
+    "url": "/client/static/js/runtime~main.84197295.js"
+  },
+  {
+    "revision": "285f175a4530b798f38b",
+    "url": "/client/static/js/2.285f175a.chunk.js"
+  },
+  {
+    "revision": "8959a1bd84486d75f2020cd944232883",
+    "url": "/client/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/client/precache-manifest.9cef26c78cccc433849679469d97e9af.js"
+  "/client/precache-manifest.502ea7bb428d359cd176bfdb812373ff.js"
 );
 
 workbox.clientsClaim();
